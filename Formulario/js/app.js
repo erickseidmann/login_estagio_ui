d09657edcb1347e1691f0a455f8edd779dc554f7
erickseidmann <erickseidmann@gmail.com>
2022-06-05 23:28:19 -0300

    coloquei ajax ainda falta retoques

---
+++ Formulario/js/app.js
@@ -0,0 +1,21 @@
+$("#cep").on("change", function(){
+    var numCep = $("#cep").val();
+    var url = "https://viacep.com.br/ws/"+numCep+"/json";
+
+    $.ajax({
+        url: url,
+        type: "get",
+        dataType: "json",
+
+        success:function(dados){
+            console.log(dados);
+            $("#cep").addClass("is-valid")
+            $("#uf").val(dados.uf);
+            $("#cidade").val(dados.localidade);
+            $("#logradouro").val(dados.logradouro);
+            $("#bairro").val(dados.bairro);
+        }
+    })
+
+   
+})
