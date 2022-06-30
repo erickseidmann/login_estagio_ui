bb8afb9f80401ef066603db916850939d0f443fd
erickseidmann <erickseidmann@gmail.com>
2022-06-30 11:58:51 -0300

    mudandod validações para jquery

--- Formulario/js/app.js
+++ Formulario/js/app.js
@@ -39,10 +39,6 @@ if ($divpj.hasClass("btn-info"))
 else
     $divpj.addClass("btn-info").removeClass("btn-outline-dark");
 });
-
- 
-
-
 var btn = document.querySelector('#pjbtn');
 var pj = document.querySelector("#pj");
 
@@ -150,46 +146,6 @@ form.addEventListener('submit', (evente) => {
     }
 
 })
-// mascara cpf, telefone, cnpj e cep usando js puro
-/*function mascara_cpf(){
-    var cpf = document.getElementById("cpf")
-    if (cpf.value.length == 3 || cpf.value.length == 7){
-        cpf.value += "."
-    } else if (cpf.value.length == 11){
-        cpf.value += "-"
-    }
-}
-
-function mascara_cnpj(){
-    var cnpj = document.getElementById("cnpj")
-    if (cnpj.value.length == 2 || cnpj.value.length == 6 || cnpj.value.length == 7){
-        cnpj.value += "."
-    } else if (cnpj.value.length == 6){
-        cnpj.value += "."
-    } else if (cnpj.value.length == 10){
-        cnpj.value += "/"
-    }else if (cnpj.value.length == 15){
-        cnpj.value += "-"
-    }
-}
-function mascara_cep(){
-    var cep = document.getElementById("cep")
-    if (cep.value.length == 6){
-        cep.value += "-"
-    }else if (cep.value.length == 2){
-        cep.value += "." 
-    }
-}
-function mascara_telefone(){
-    var telefone = document.getElementById("telefone")
-    if (telefone.value.length == 0 || telefone.value.length == 1){
-        telefone.value += "("
-    }else if (telefone.value.length == 3){
-        telefone.value += ")"
-    }else if (telefone.value.length == 9){
-        telefone.value += "-"
-    }  
-}*/
 // mascara cpf, telefone, cnpj e cep usando jquery
 $(document).ready(function () {
     $("#cep").mask('00.000-000', { placeholder: '__.___-___' });
@@ -199,36 +155,7 @@ $(document).ready(function () {
 })
 
 
-// validador do cpf 
-/*function TestaCPF(strCPF) {
-    var Soma;
-    var Resto;
-    Soma = 0;
-  if (strCPF == "00000000000") return false;
-
-  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-  Resto = (Soma * 10) % 11;
-
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-
-  Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-    Resto = (Soma * 10) % 11;
-
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-    return true;
-}
-var strCPF = document.getElementById('cpf');
-var cpf = strCPF;
-
-if (cpf.value == false) {
-    cpf.classList.add("errorInput");
-    cpf.focus()
-}else{
-    cpf.classList.remove("errorInput")
-}*/
+// validador do cpf/ termos / cnpj jquery  
 
 $(document).ready(function () {
     $("#form").validate({
