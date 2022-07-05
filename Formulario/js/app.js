ff9095262509135675e67c47da5491546b51b964
erickseidmann <erickseidmann@gmail.com>
2022-07-05 09:33:41 -0300

    estetica da data

--- Formulario/js/app.js
+++ Formulario/js/app.js
@@ -168,7 +168,7 @@ $(document).ready(function () {
     $("#tele").mask('(00)0000-0000', { placeholder: '(__)_____-____' });
     $("#cpf").mask('000.000.000-00', { placeholder: '___.___.___-__' });
     $("#cnpj").mask('00.000.000/0000-00', { placeholder: '__.___.___/____-__' })
-    $("#data").mask('00/00/0000', { placeholder: '__/__/____' })
+    $("#data").mask('00/00/0000', { placeholder: ' ___/__/____   📅' })
 })
 
 // validador do cpf/ termos / cnpj jquery  
